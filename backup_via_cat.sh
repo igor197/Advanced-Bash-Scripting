ab1b182be054adfd2a7f2519f7e0133ac38e8cc7
igor197 <igor1975@yandex.ru>
2022-08-17 12:03:22 +0300

    Added a description of the task

--- backup_via_cat.sh
+++ backup_via_cat.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env /bin/bash
 
-#PATH_BACKUP=/root/My_scripts/dirbackup
+#Write a script that backs itself up, that is, copies itself to a file named
+#backup.sh. Hint: Use the cat command and the appropriate positional parameter.
 
 while :
+
 do
 if [ $# -ne 1 ]
 then 
-    echo "Не указано имя файла для создания его резервной копии \
-или указано больше одного параметра"  
-    exit  
+  echo "Specify the parameter, file name"  
+  exit  
 else
-    file=$(echo $1 | cut -d. -f1)
-    extension=$(echo $1 | cut -d. -f2)
-    #touch $1_backup
-    echo $file
-    echo $extension
-    #touch $file'_backup'.$extension
-    cat $1 > $file'_backup'.$extension
-    break
+  file=$(echo $1 | cut -d. -f1)
+  extension=$(echo $1 | cut -d. -f2)
+  cat $1 > $file'_backup'.$extension
+  break
 fi
+
 done
+
 echo "Did it"
+
 exit 0
   
 
--- until_loop.sh
+++ until_loop.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env /bin/bash
 
+#Convert the for loops in Example 11-1 to while loops. Hint: store the data in an array and step through
+#the array elements.
+
 solar_system=( Mercury Venus Earth Mars Jupiter Saturn Uranus Neptune Pluto )
 
 n=0

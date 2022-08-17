b4aad1a4e7a15fe52cbdcfa6971e43c6a9612f6f
igor197 <igor1975@yandex.ru>
2022-08-17 11:12:11 +0300

    Complete while_loop.sh

--- while_loop.sh
+++ while_loop.sh
@@ -1,1 +1,14 @@
 #!/usr/bin/env /bin/bash
+
+solar_system=( Mercury Venus Earth Mars Jupiter Saturn Uranus Neptune Pluto )
+
+n=0
+count=${#solar_system[@]}
+
+while [[ $n -le $count ]] 
+do
+  echo ${solar_system[$n]}
+  n=$(( $n + 1 ))
+done 
+
+exit 
